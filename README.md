556d7a0dfc177d3878b3259892a6547cf83762f9
Yan Gameiro <yan.gameiro@gmail.com>
2021-09-30 22:15:07 -0300

    add some style to the movies list and better README.md

--- README.md
+++ README.md
@@ -7,6 +7,13 @@ This is a project created using techs like Serverless Framework, AWS Lambda, API
 - Clone this repo
 - Enter `serverless` folder
 - Install dependencies with `npm install`
-- create a .env file following the .env.example [help on THEMOVIEDB_API_KEY](https://developers.themoviedb.org/3/getting-started/introduction)
+- Create a .env file following the .env.example [help on THEMOVIEDB_API_KEY](https://developers.themoviedb.org/3/getting-started/introduction)
 - Install and configure AWS CLI [docs for help](https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/serverless-sam-cli-install.html)
 - Run deploy command `serverless deploy --verbose`
+
+## Setup frontend
+
+- Clone this repo
+- Enter `frontend-moviesy` folder
+- Install dependencies with `yarn install`
+- Strart the app with `yarn start`
--- frontend-moviesy/src/components/ListMovies/index.js
+++ frontend-moviesy/src/components/ListMovies/index.js
@@ -1,12 +1,57 @@
+import styled from "styled-components";
+import defaultImg from "./no-image.png";
+
+const Wrapper = styled.div`
+  max-width: 1450px;
+  padding: 2em;
+  margin-top: 2em;
+  margin-left: auto;
+  margin-right: auto;
+  background: #f7f7f7;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  border-radius: 30px;
+`;
+
+const MovieCard = styled.div`
+  display: flex;
+  width: 180px;
+  height: 375px;
+  flex-direction: column;
+  border: 1px solid #e4e4e4;
+  border-radius: 10px;
+  margin-bottom: 16px;
+  justify-content: space-around;
+`;
+
+const MovieImg = styled.div`
+  width: 175px;
+  height: 275px;
+  margin-left: auto;
+  margin-right: auto;
+  background-image: url(https://image.tmdb.org/t/p/w500/${(props) =>
+    props.link});
+  background-size: auto 100%;
+  background-repeat: no-repeat;
+  background-position: center center;
+`;
+
+const CardInfo = styled.div`
+  text-align: center;
+`;
+
 const ListMovies = ({ movies }) => {
   return (
-    <ul>
+    <Wrapper>
       {movies.map((movie, key) => (
-        <li
-          key={key}
-        >{`Original Title: ${movie.original_title} --- Rate: ${movie.vote_average}`}</li>
+        <MovieCard key={key}>
+          <MovieImg link={movie.poster_path}> </MovieImg>
+          <CardInfo> {`rate: ${movie.vote_average}`}</CardInfo>
+          <CardInfo>{`${movie.title}`}</CardInfo>
+        </MovieCard>
       ))}
-    </ul>
+    </Wrapper>
   );
 };
 
